6011a52ade187127dec55d4ff40a5b551e04b56d
aluno <aluno@labivj.info>
2018-03-14 20:32:09 -0300

    Exercicios

--- WebService/MyWebService.asmx.cs
+++ WebService/MyWebService.asmx.cs
@@ -18,9 +18,47 @@ namespace WebService
     {
 
         [WebMethod]
-        public string HelloWorld()
+        public string HelloWorld(string nome, int idade)
         {
-            return "Hello World";
+            return "Olá "+nome+" sua idade é "+idade+" anos";
+        }
+
+        [WebMethod]
+        public decimal Calcula(decimal n1, decimal n2, string op)
+        {
+            if (op == "+")
+                return n1 + n2;
+            else if (op == "-")
+                return n1 - n2;
+            else if (op == "*")
+                return n1 * n2;
+            else if (op == "/")
+                return n1 / n2;
+            else            
+                throw new Exception("O operador é inválido!");            
+        }
+
+        [WebMethod]
+        public double Temp(double temp, string grau)
+        {
+            double c = 0;
+            double f = 0;
+            if(grau.ToLower() == "f")
+            {
+                f = temp;
+                c = (f - 32) / 1.8;
+                return c;
+            }
+            else if(grau.ToLower() == "c")
+            {
+                c = temp;
+                f = (c * 1.8) + 32;
+                return f;
+            }
+            else
+            {
+                throw new Exception("A escala é inválida, valores válidos: 'F' ou 'C'");
+            }
         }
     }
 }
